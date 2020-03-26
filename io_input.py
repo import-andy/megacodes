8bd2dc43e3d7fcc5538d8f267df7e5393f936372
import-andy <62716884+import-andy@users.noreply.github.com>
2020-03-26 20:06:14 +0200

    Add files via upload
    Done homework exercise from the book 'Byte of Python' - Chapter Input from user
    Checking whether a text is a palindrome should also ignore punctuation, spaces and case. For example, "Rise to vote, sir." is also
    a palindrome but our current program doesn't say it is. Can you improve the above program to recognize this palindrome?

---
+++ io_input.py
@@ -0,0 +1,23 @@
+def reverse(text):
+    return text[::-1]
+
+
+def is_palindrome(text):
+    return text == reverse(text)  # Returns True or False
+
+
+input_text = input('Input text: ')
+
+# Making all cases able to be compared
+input_text = input_text.casefold()
+
+# Removing punctuation
+punctuation_signs = [',', '.', '!', '?', '/', '\\', ':', ' ']
+for x in punctuation_signs:
+    input_text = input_text.replace(x, '')
+
+
+if is_palindrome(input_text):
+    print('Yes, it\'s palindrome!')
+else:
+    print('No, it isn\'t palindrome...')
